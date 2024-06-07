bb16222619aa2614bf8890ee599bb6c2cf74879b
Otavio-Senger <senger.otavio@gmail.com>
2024-06-07 20:53:48 -0300

    pronto

--- formulario/script.js
+++ formulario/script.js
@@ -1,14 +1,14 @@
 var botao = document.getElementById("botao");
 var verificacao = document.getElementById("form");
 
-verificacao.addEventListener("submit", function(){
+verificacao.addEventListener("submit", function(event){
     //verifica se o formulário é válido
+    event.preventDefault();//caso o formulário seja inválido, impede o envio
     if(this.checkVisibility()){
-        event.preventDefault();//caso o formulário seja inválido, impede o envio
-        
-        botao.addEventListener("click", function(){
+        var nome = document.getElementById('nome').value;
+        localStorage.setItem('nomeusuario', nome);
         window.location.href = 'file:///C:/Users/Ot%C3%A1vio/Desktop/jogoAdvivinha%C3%A7%C3%A3o/jogo/jogo.html'
- });
+
     }
 });
 
