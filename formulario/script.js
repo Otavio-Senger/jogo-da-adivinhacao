7d9921e522b22c3000f3854e572bc37656b5efff
Otavio-Senger <166336227+Otavio-Senger@users.noreply.github.com>
2024-06-19 18:42:17 -0300

    Add files via upload

---
+++ formulario/script.js
@@ -0,0 +1,21 @@
+var botao = document.getElementById("botao");
+var verificacao = document.getElementById("form");
+
+window.onload = function(){
+    document.getElementById('nome').focus();
+};
+
+verificacao.addEventListener("submit", function(event){
+    //verifica se o formulário é válido
+    // event.preventDefault();//caso o formulário seja inválido, impede o envio
+    if(this.checkValidity()){
+        var nome = document.getElementById('nome').value;
+        localStorage.setItem('nomeusuario', nome);
+        // window.location.href = '../jogo/jogo.html'
+
+
+    }
+});
+
+
+
